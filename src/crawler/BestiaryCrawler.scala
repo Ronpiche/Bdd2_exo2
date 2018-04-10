e430a37493d9f912631699e830ae2c733b84820a
Gaetan MARTIN <gatus42@gmail.com>
2018-04-10 12:26:13 -0400

    Added range attack to solar

--- src/crawler/BestiaryCrawler.scala
+++ src/crawler/BestiaryCrawler.scala
@@ -10,15 +10,11 @@ class BestiaryCrawler extends AbstractCrawler {
 
   override def crawlLink(link: String): Unit = {
     try {
-      var conn = Jsoup.connect(link)
-      var doc = conn.get()
-
+      var doc = Jsoup.connect(link).get()
       var hrefLinks = doc.getElementById("monster-index-wrapper").select("a[href]")
-
       for (i <- 0 until hrefLinks.size()) {
         monsterLinks.append(hrefLinks.get(i).attr("abs:href"))
       }
-
     } catch {
       case e: Exception => print(e)
     }
--- src/monsters/Solar.scala
+++ src/monsters/Solar.scala
@@ -1,5 +1,7 @@
 package monsters
 
+import utils.Dice
+
 /**
   * Solar
   * http://www.d20pfsrd.com/bestiary/monster-listings/outsiders/angel/solar/
@@ -12,4 +14,18 @@ class Solar extends AbstractMonster {
   override var init: Int = 9
   override var name: String = "Angel Solar"
   override var meleeHit: Int = 22
+  var rangeHit: Int = 22
+
+  /**
+    * Rnage attack
+    * @param opponent target
+    */
+  def rangeAttack(opponent: AbstractMonster): Unit = {
+    for (_ <- 0 to nbAttacks) {
+      Dice.roll() // Roll the hit chance
+      if (Dice.criticalSuccess || Dice.value + rangeHit > opponent.armor) {
+        opponent.hurt(damages)
+      }
+    }
+  }
 }
